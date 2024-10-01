653ab0c14a5c10f9bdc8f05ca2d6923632a71d2c
黄铭涛 <1037827920@qq.com>
2024-10-01 12:00:31 +0800

    add .gitignore

--- rust_single_thread/target/debug/deps/rust_single_thread-12dbedeb85c819a4.d
+++
@@ -1,5 +0,0 @@
-/home/hmt/Documents/web-server/rust_single_thread/target/debug/deps/rust_single_thread-12dbedeb85c819a4: src/main.rs
-
-/home/hmt/Documents/web-server/rust_single_thread/target/debug/deps/rust_single_thread-12dbedeb85c819a4.d: src/main.rs
-
-src/main.rs:
--- rust_single_thread/target/debug/rust_single_thread.d
+++
@@ -1,1 +0,0 @@
-/home/hmt/Documents/web-server/rust_single_thread/target/debug/rust_single_thread: /home/hmt/Documents/web-server/rust_single_thread/src/main.rs
